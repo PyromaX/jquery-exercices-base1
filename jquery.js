9fe658d82f033597de7c614b2cd888a6438ad5ab
PyromaX <jeux.pyromaxx@gmail.com>
2017-04-14 11:28:56 +0200

    save2

--- jquery.js
+++ jquery.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
   $("#bouton1").click(function(){
-      $("#texte").hide();
-  });
-  $("#bouton2").click(function(){
-      $("#texte").show();
+    var name = $("#nom").val();
+    var Age = $("#age").val();
+    $("#yolo").append("bonjour " + name + " !");
+
+    //LECTEUR
+
+    var musique = "<audio preload=\"auto\" id=\"Psycho\" src=\"./Psycho.mp3\"></audio><audio preload=\"auto\" id=\"Schubert\" src=\"./Schubert.mp3\"></audio>";
+    $("#lecteur").append(musique);
+    var player = document.querySelector((Age < 18) ? "#Schubert" : "#Psycho");
+    player.play();
+
   });
 });
